26e52ce011b71c8f6eef96c0c3e5a5b05ce123b8
olga_demina <olga.demina1410@gmail.com>
2024-10-28 13:00:46 +0100

    feat: operatore del resto, isnan e random

---
+++ parte-2/js/script.js
@@ -0,0 +1,46 @@
+// 1. Operatore del resto della divisione
+// const num1 = 17;
+// const num2 = 7;
+
+// console.log(num1 % num2);
+
+// 2. Calcolo del numero random
+// const rndNum = Math.random(); // ritorna numero tra 0 e 1 con tanti cifre dopo la virgola
+// console.log(rndNum);
+
+// Numero random tra 0 e 9
+// const rndNum = Math.random() * 10;
+// const rndNumInt = Math.floor(rndNum)
+// console.log(rndNum, rndNumInt);
+
+// Numero random da 1 a 6
+// const rndNum = Math.floor(Math.random() * 6) + 1;
+// console.log(rndNum);
+
+
+// 
+
+// const num1 = 12;
+// const num2 = "Ciao";
+// 
+// const result = num1 / num2; // NaN
+// console.log(result);
+
+// console.log(isNaN(num2));
+// const myResult = isNaN(num1);
+// console.log(myResult);
+
+// const userAge = "18";
+// 
+// console.log("stringa userAge è NaN?", isNaN(userAge));
+// 
+// console.log(parseInt(userAge));
+
+
+// Come chiedere input da parte dell'utente
+// const username = prompt("Come ti chiami?");
+// console.log(username);
+// const userNumber = prompt("Dimmi un numero da 1 a 10"); // string
+// console.log(userNumber);
+// const userNumberAsInt = parseInt(userNumber); // number
+
